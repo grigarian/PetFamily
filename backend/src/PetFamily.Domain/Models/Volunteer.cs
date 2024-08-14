776594e1e40a0e92c466948c3cc5c881cef16b87
grigarian <grigarian24@mail.ru>
2024-08-14 21:26:30 +0500

    ID-B-3

---
+++ backend/src/PetFamily.Domain/Models/Volunteer.cs
@@ -0,0 +1,27 @@
+﻿namespace PetFamily.Domain.Models
+{
+    public class Volunteer
+    {
+        public Guid ID { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public int WorkExpirience { get; private set; }
+
+        public int CountPetFoundHome { get; private set; }
+
+        public int CountPetLookingForHome { get; private set; }
+
+        public int CountPetBeingTreated { get; private set; }
+
+        public string PhoneNumber { get; private set; }
+
+        public List<SocialNetwork> SocialNetworks { get; private set; }
+
+        public List<Requisite> Requisites { get; private set;}
+
+        public List<Pet> Pets { get; private set; }
+    }
+}
