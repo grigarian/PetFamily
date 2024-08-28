7aaf3618931b96df3da596d45a802c78d8a9248a
grigarian <grigarian24@mail.ru>
2024-08-28 12:37:23 +0500

    Исправил коллекцию социальных сетей и реквизитов

--- backend/src/PetFamily.Domain/Models/Shared/Requisites.cs
+++ backend/src/PetFamily.Domain/Models/Shared/Requisites.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PetFamily.Domain.Models.Shared
+﻿namespace PetFamily.Domain.Models.Shared
 {
-    public class Requisites
+    public record Requisites
     {
-        private Requisites()
-        {
-            
-        }
-        public IReadOnlyList<Requisite> Requisite { get; private set; }
+        private Requisites() {}
+        private Requisites(ICollection<Requisite> requisites) => Requisite = requisites.ToList();
+        public IReadOnlyList<Requisite> Requisite { get; }
+        public static Requisites Create(ICollection<Requisite> requisites) => new(requisites);
     }
 }
--- backend/src/PetFamily.Domain/Models/VolunteerModel/SocialNetworks.cs
+++ backend/src/PetFamily.Domain/Models/VolunteerModel/SocialNetworks.cs
@@ -4,6 +4,12 @@ namespace PetFamily.Domain.Models.VolunteerModel
 {
     public record SocialNetworks
     {
-        public IReadOnlyList<SocialNetwork> SocialNetwork { get; private set; }
+        private SocialNetworks() {}
+
+        private SocialNetworks(ICollection<SocialNetwork> socialNetworks) => SocialNetwork = socialNetworks.ToList();
+
+        public IReadOnlyList<SocialNetwork> SocialNetwork { get; }
+
+        public static SocialNetworks Create(ICollection<SocialNetwork> socialNetworks) => new(socialNetworks);
     }
 }
